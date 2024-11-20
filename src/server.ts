b0a3f44dbae896a9967b74c3bac29a857d368e01
faysal-backend-developer <developer.faysalhasan@gmail.com>
2024-11-20 11:42:41 +0600

    Hook Check

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import dbConnection from './db'
 const server = async () => {
   try {
     await dbConnection(config.db_url as string)
-    app.listen(config.port, () => {
+    app..listen(config.port, () => {
       console.log(`server listening on ${config.port}`)
     })
   } catch (error) {
